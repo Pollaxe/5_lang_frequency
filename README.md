4162549cc35ed69ba1ea4bb4cacff945c9cc073e
Pollaxe <bobrax@yandex.ru>
2018-01-26 12:13:02 +0300

    version one

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Script to print 10 most frequent words from text file.
+
+# Quickstart
+
+This was tested on windows, python 3.5+.
+You can just type in command console: 
+```
+python lang_frequency.py <file name>
+```
+
+# Output example
+
+From text file which contains something like:
+```
+раз два два три три три четыре четыре четыре четыре one two десять десять десять десять пять шесть шесть
+двадцать двадцать двадцать двадцать forty forty forty forty forty forty forty
+```
+
+Output will be:
+```
+>forty
+>двадцать
+>десять
+>четыре
+>три
+>два
+>шесть
+>one
+>two
+>пять
+```
+
+# Functions
+
+As alternative, you can import functions from this script to use in your project:
+```python
+load_data(file_path) # just loads data from text file.
+```
+
+```python
+get_most_frequent_words(text) # returns list which contains words ordered from most frequent to least frequent with quantity.
+```
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,38 @@
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path) as content:
+        text = content.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    most_frequent_words = dict()
+    most_frequent_words_list = []
+    for word in text.split():
+        if word not in most_frequent_words:
+            most_frequent_words[word] = 1, word
+        else:
+            buffer = most_frequent_words[word][0]+1
+            most_frequent_words[word] = buffer, word
+    for word in most_frequent_words:
+        most_frequent_words_list += [(most_frequent_words[word])]
+    list.sort(most_frequent_words_list, key=lambda x: (-x[0], x[1]))
+    return most_frequent_words_list
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_path = sys.argv[1]
+        text = load_data(file_path)
+    except FileNotFoundError:
+        sys.exit('Файл не найден, попробуйте еще раз.')
+    except IndexError:
+        sys.exit('Используйте синтакс: "python lang_frequency.py <filename>"')
+    most_frequent_words_list = get_most_frequent_words(text)
+    for word in range(10):
+        try:
+            print(most_frequent_words_list[word][1])
+        except IndexError:
+            sys.exit()
